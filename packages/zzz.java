0593c7018fbebe7a4b25e2819d0139d877a56ac8
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-01-25 00:44:10 +0530

    updated

--- packages/zzz.java
+++
@@ -1,6 +0,0 @@
-package harry;
-public class zzz {
-    public static void main(String[] args) {
-        System.out.println("hii");
-    }
-}
--- packages/zzz1.java
+++
@@ -1,9 +0,0 @@
-package harry;
-
-public class zzz1{
-    public static  void main(String[] args) {
-        System.out.println("hii harry1");
-
-    }
-}
-    
