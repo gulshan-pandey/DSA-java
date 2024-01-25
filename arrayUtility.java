eb5ab1293cc53eaac31c8131430e90e160454a18
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-01-25 13:36:05 +0530

    updated

--- arrayUtility.java
+++ arrayUtility.java
@@ -8,8 +8,9 @@ public class arrayUtility {
         int size = sc.nextInt();
 
         int[] arey = new int[size];
-        System.out.println("enter the element of array");
+
         for (int i = 0; i < arey.length; i++) {
+            System.out.println("enter the element "+(i+1) + " : ");
             arey[i] = sc.nextInt();
 
         }
--- temp.java
+++ temp.java
@@ -4,18 +4,8 @@ public class temp {
    
     public static void main(String[] args) {
 
-     
-int [] arr={24,6,2,89,73,90};
-        int temp= arr[0];
-        
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]<temp){
-                temp=arr[i];
-            }
-          
-        }
-        System.out.println(temp);
-        
 
-    }
+
+
+
 }
