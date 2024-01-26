7114e794645218f2ea49df229ad076171779e761
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-01-27 02:45:48 +0530

    up

--- temp.java
+++ temp.java
@@ -28,13 +28,17 @@ public class temp{
 
        //absolut number
 
-       System.out.println("enter any number for makint its absolute number : ");
-       int num5 = sc.nextInt();
+    //    System.out.println("enter any number for its absolute number : ");
+    //    int num5 = sc.nextInt();
        
-       System.out.println(num5>=0? num5: -num5);
+    //    System.out.println(num5>=0? num5: -num5);
 
+    // WAP to categorize the students number in high moderate or low
 
+    System.out.println("enter the number of student : ");
+    int numb = sc.nextInt();
 
+    System.out.println(numb>80? "High": (numb>50&&numb<80 ? "moderate": "Low") );
 
     }
 }
