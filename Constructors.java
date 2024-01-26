bdea305cc492de85bac690ad8c9130d1da2a1dc7
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-01-26 15:46:40 +0530

    updated

--- Constructors.java
+++ Constructors.java
@@ -1,72 +1,92 @@
-class human{
+class human {
 
     private String name;
     private int age;
 
-    public human(){  //this is constructor and it calls itself whenever the new object is initialized
-        name="kaliaa"; //and the constructors helps us to give the default values
-        age=21;
+    public human() { // this is constructor and it calls itself whenever the new object is
+                     // initialized
+        name = "kaliaa"; // and the constructors helps us to give the default values
+        age = 21;
     }
 
-    public human(String n){ //this is constructor overloading
-        name=n;
-    }
-   
-    public human(int a ,String n){ //this is constructor overloading
-        age= a;
-        name= n;
+    public human(String n) { // this is constructor overloading
+        name = n;
     }
 
-    public void setName(String n){
-        name =n;
+    public human(int a, String n) { // this is constructor overloading
+        age = a;
+        name = n;
     }
-    public String getName(){
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String getName() {
         return name;
     }
 
-    public int getAge(){
+    public int getAge() {
         return age;
     }
 }
 
-
-
-class worker{
+class worker {
     private int salary;
     private String name;
 
-    public worker(){ //initializing constructor that takes no argument...
-        salary =12;
+    public worker() { // initializing constructor that takes no argument...
+        salary = 12;
     }
 
-    public int getSalary(){
-        return salary ;
+    public int getSalary() {
+        return salary;
     }
 }
 
+class Car {
+    int noOfWheels;
+    String color;
+    float maxSpeed;
+    int noOfSeats;
+    float fuel;
 
+    Car(String color) {
+        this.color = color;
+        noOfSeats = 5;
+        noOfWheels = 4;
+        fuel = 3;
+        maxSpeed = 150;
 
+    }
+
+    Car() {
+        this("Black"); // it is constructor chaining ,in which the constructor call its own version to
+                       // get the work done ,and all the properties of the another constructor will be
+                       // set
+        fuel = 5; // default constructor caller will get 5 ltr of fuel
+    }
+
+}
 
 public class Constructors {
     public static void main(String[] args) {
-        
+
         human person = new human();
         System.out.println(person.getName() + " : " + person.getAge());
-     
-        human person3 = new human ( "gaurav");
+
+        human person3 = new human("gaurav");
         System.out.println(person3.getName() + " : " + person3.getAge());
-    
-        human person2 =new human( 18,"ghoda");
+
+        human person2 = new human(18, "ghoda");
         System.out.println(person2.getName() + " : " + person2.getAge());
 
-    
-    
-        worker empl1= new worker();
+        worker empl1 = new worker();
         System.out.println("the default slary of empl1 is  : " + empl1.getSalary());
 
+        Car swift = new Car("red");
+
+        System.out.println(swift.color);
     }
-    
-
-
 
 }
