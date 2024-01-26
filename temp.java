f773c7b895e4d9d86b35d3ba9f2ccc8354defb9c
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-01-27 02:16:05 +0530

    pushed

--- temp.java
+++ temp.java
@@ -1,8 +1,19 @@
+import java.util.*;
+
 public class temp{
 
     public static void main(String[] args) {
         
+        System.out.println("enter 1st no. : ");
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
         
+        System.out.println("enter 2nd no. : ");
+        int num2 = sc.nextInt();
+
+        System.out.println("the greater number is : " + (num1>num2? num1:num2));
+
+
 
     }
 }
