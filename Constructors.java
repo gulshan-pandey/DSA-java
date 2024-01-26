efe68d223a003117027b8017fe088550b77d2720
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-01-26 20:51:55 +0530

    updated

--- Constructors.java
+++ Constructors.java
@@ -3,9 +3,10 @@ class human {
     private String name;
     private int age;
 
-    public human() { // this is constructor and it calls itself whenever the new object is
+    public human() { // this is constructor and it calls itself whenever the new object is and has no
+                     // return type
                      // initialized
-        name = "kaliaa"; // and the constructors helps us to give the default values
+        name = "kaliaa"; // the constructors helps us to give the default values
         age = 21;
     }
 
@@ -51,7 +52,7 @@ class Car {
     int noOfSeats;
     float fuel;
 
-    static int noOfCarSold;  // this is static that how much car has been sold 
+    static int noOfCarSold; // this is static that how much car has been sold
 
     Car(String color) {
         this.color = color;
@@ -68,21 +69,28 @@ class Car {
                        // set
         fuel = 5; // default constructor caller will get 5 ltr of fuel
     }
-      
-    //the static block runs first of all !!
-     //static is code block and it is called only once doesnt matter how mant objects u have made
-    static{  
-     noOfCarSold=0;
-     System.out.println(" I am in static block,and run only once");
+
+    // the static block runs first of all !!
+    // static is code block and it is called only once doesnt matter how mant
+    // objects u have made
+    static {
+        noOfCarSold = 0;
+        System.out.println(" I am in static block,and run only once");
     }
 
-    //normal codeblock will run as many times as the objects has been made
-    { //normal code block
-      noOfCarSold++;
-      System.out.println("I am in initialization block,and i will run as many times as much objects it has");
+    // normal codeblock will run as many times as the objects has been made
+    { // normal code block
+        noOfCarSold++;
+        System.out.println("I am in initialization block,and i will run as many times as much objects it has");
     }
 
 }
+
+
+
+
+
+
 
 public class Constructors {
     public static void main(String[] args) {
@@ -97,15 +105,17 @@ public class Constructors {
         System.out.println(person2.getName() + " : " + person2.getAge());
 
         worker empl1 = new worker();
-        System.out.println("the default slary of empl1 is  : " + empl1.getSalary() +" \n\n");
+        System.out.println("the default slary of empl1 is  : " + empl1.getSalary() + " \n\n");
 
         Car swift = new Car("red");
         Car thar = new Car();
 
-
-
         System.out.println(swift.color);
-        System.out.println(thar.color);
+        System.out.println(thar.color + " \n\n ");
+
+
+
+       
     }
 
 }
---
+++ LibraryManagementSystem.java
@@ -0,0 +1,74 @@
+
+class Book {
+    static int totalNoOfBooks;
+    
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean isborrowed;
+    
+    public void borrowBook() {
+        if (isborrowed) {
+            System.out.println(title + " : the book is already borrowed");
+        } else {
+            System.out.println("enjoy ur book " + title );
+            totalNoOfBooks--;
+            this.isborrowed=true;
+        }
+        
+    }
+    
+    public void returnBook() {
+        if (isborrowed) {
+            System.out.println("thanks for reading the book : " + title );
+            totalNoOfBooks++;
+            isborrowed= false;
+        } else {
+            System.out.println(title+ " : the book is already in the library");
+            
+        }
+    }
+    
+    static {
+        totalNoOfBooks = 0;
+    }
+    {
+        totalNoOfBooks++; //means that whenever the book object is created ,the number of book get increased by one
+    }
+    
+    static int getTotalNoOfBooks(){
+        return totalNoOfBooks;
+    }
+    
+    Book(String isbn, String title, String author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
+
+    Book(String isbn) {
+        this(isbn, "unknown", "unknown"); // constructor chaining!
+    }
+}
+public class LibraryManagementSystem {
+public static void main(String[] args) {
+    
+    
+    System.out.println("welcome to the library!!!");
+    Book dsa =new Book("1" , "dsa", "xyz");
+    
+    Book book2 = new Book("2");
+    
+    dsa.borrowBook();
+    System.out.println(Book.getTotalNoOfBooks()); //static method can be called either way
+    book2.borrowBook();
+    System.out.println(book2.getTotalNoOfBooks());
+    
+    dsa.borrowBook();
+    dsa.returnBook();
+        book2.returnBook();
+        book2.returnBook();
+        System.out.println(Book.getTotalNoOfBooks());
+ 
+    }
+}
