6ccb0d14ae33df9b51f9733159b510cd432fe880
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-01-27 02:37:57 +0530

    updated

--- temp.java
+++ temp.java
@@ -3,15 +3,36 @@ import java.util.*;
 public class temp{
 
     public static void main(String[] args) {
-        
-        System.out.println("enter 1st no. : ");
         Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
         
-        System.out.println("enter 2nd no. : ");
-        int num2 = sc.nextInt();
+        // System.out.println("enter 1st no. : ");
+        // int num1 = sc.nextInt();
+        
+        // System.out.println("enter 2nd no. : ");
+        // int num2 = sc.nextInt();
 
-        System.out.println("the greater number is : " + (num1>num2? num1:num2));
+        // System.out.println("the greater number is : " + (num1>num2? num1:num2));
+
+        // //odd even ternary
+
+        // System.out.println("welcome to odd even ");
+
+        // System.out.println("enter the number 1: ");
+        // int num3 = sc.nextInt();
+        
+
+        // System.out.println("enter the number 2: ");
+        // int num4 = sc.nextInt();
+
+        // System.out.println("the even number is : " + (num3%2==0?num3:num4));
+
+       //absolut number
+
+       System.out.println("enter any number for makint its absolute number : ");
+       int num5 = sc.nextInt();
+       
+       System.out.println(num5>=0? num5: -num5);
+
 
 
 
