f2ebf9f31589d4b582a9b18bca1242f782d2e209
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-01-31 02:16:34 +0530

    added

--- temp.java
+++ temp.java
@@ -1,9 +1,25 @@
 import java.util.*;
 
 public class temp{
+  public static long factorial(long num){
+    if(num==0){
+        return 1;
+    }
+    else{
+        num= num*factorial(num-1);
+        return num;
+    }
+
+  }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number you want the factorial of !");
+        int fac = sc.nextInt();
+        long total= factorial(fac);
+        System.out.println(total);
+
    
      
     }
