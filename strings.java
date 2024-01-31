951c796f2822265ad7656c43cfcb63c744b9e74c
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-02-01 04:06:42 +0530

    added

--- strings.java
+++ strings.java
@@ -1,26 +1,38 @@
 
 import java.util.Scanner;
+
 public class strings {
     public static void main(String[] args) {
-        String name ="rohan";
-        String name2=new String("munna"); //another way to define string
-//        System.out.print("the name is: ");
-//        System.out.print(name );
-        //here remember that the println is used for the printing next words with new line, but the 'print' does not include the new line and prints the next words in the same line
+        String name = "rohan";
+        String name2 = new String("munna"); // another way to define string
+        // System.out.print("the name is: ");
+        // System.out.print(name );
+        // here remember that the println is used for the printing next words with new
+        // line, but the 'print' does not include the new line and prints the next words
+        // in the same line
 
-        //more diff ways to print things
-        int m=66;
-        float n=55.333f;
-        System.out.printf("the values of m was %d and the value of n is %8.3f",m,n);
-        //same as C lang ryt  remember that here %8.3f is mean that the number will occupy 8 digit spaces and the .3 is mean that it will upto 3 decimal point???
-        //for taking the string as an input
-       //greetings
+        // more diff ways to print things
+        int m = 66;
+        float n = 55.333f;
+        System.out.printf("the values of m was %d and the value of n is %8.3f", m, n);
+        // same as C lang ryt remember that here %8.3f is mean that the number will
+        // occupy 8 digit spaces and the .3 is mean that it will upto 3 decimal point???
+        // for taking the string as an input
+        // greetings
 
         System.out.println("hello how are you");
-        Scanner s=new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         String ans = s.nextLine();
-        System.out.println(ans +"  as well");
+        System.out.println(ans + "  as well");
 
+
+        //use string builder to make a string
+        //string builder is better than the string making method,is it fastest ,reduces memory storage...
+        StringBuilder sb = new StringBuilder("this the demo");
+        sb.append(53);
+        sb.append(" i am the destroyer");
+        sb.append(3000.3000);
+        System.out.println(sb.toString());
 
     }
 
--- temp.java
+++ temp.java
@@ -1,24 +1,11 @@
 import java.util.*;
 
 class temp {
-  public static boolean palinStr(String str) {
-    
-    if (str.length() <= 1) {
-      return true;
-    }
-    int length = str.length() - 1;
-    if (str.charAt(0) != str.charAt(length)) {
-      return false;
-    }
-    String strng = str.substring(1, length); // the length is inclusive
-    return palinStr(strng);
 
-  }
+  
 
   public static void main(String[] args) {
-    System.out.println("weclome to String palindrome checker ! \n\n please enter the word to check if it is palindrome ");
-    Scanner sc = new Scanner(System.in);
-    String word = sc.next();
-    System.out.println("your word is " + ((palinStr(word)) ? " palindrome" : "not a palindrome word"));
+
   }
 }
+ 
