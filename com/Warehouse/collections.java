220f755e466b48438ef58a3e2f0025a3813a9741
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-02-04 01:44:37 +0530

    added

--- com/Warehouse/collections.java
+++ com/Warehouse/collections.java
@@ -9,7 +9,9 @@ public class collections {
         
         splender.color="green"; //accessible
         splender.tyres=2; // accessible
+
         // splender.model = 234 //it is not accessible is it default and is accessible within the same package
+        
         // splender.costOfPurchase =234; // cant be accessed, is is private
 
         System.out.println(splender);
