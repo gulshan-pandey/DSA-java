c9fde5a83943e9088d04101cf8024e1fd9480e8d
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-02-10 21:54:34 +0530

    updated

--- Exceptions.java
+++ Exceptions.java
@@ -62,59 +62,61 @@ public class Exceptions{
 
 
  //WAP to keep enter the index from the user until get correct index
- 
 
 
-        // int[] arr = new int[4];
+
+        int[] arr = new int[4];
         
-        // arr[0] = 44;
-        // arr[1] = 44;
-        // arr[2] = 45;
-        // arr[3] = 90;
+        arr[0] = 44;
+        arr[1] = 44;
+        arr[2] = 45;
+        arr[3] = 90;
      
         
-        // for (int i =0;i<5;i++){
-        //     try {
-        //     System.out.println("Enter an index:");
-        //     int ind = sc.nextInt();
-        //         System.out.println("Welcome");
-        //         try {
-        //             System.out.println(arr[ind]);
-        //         } catch (ArrayIndexOutOfBoundsException e) {
-        //             System.out.println("Array out of bounds");
-        //         }
-        //     }catch (InputMismatchException e) {
-        //         System.out.println("Input mismatch occurred. Please enter an integer value.");
-        //         // Clearing the invalid input
-        //         sc.next();
-        //     }
-            
-        //     System.out.println("Thanks");
-        // }
-
-    
-            for(int i =0;i<5;i++){
-            
-            System.out.println("enter the number1 : ");
-            try{
-            int x = sc.nextInt();
-            System.out.println("enter the second number : ");
-            int y = sc. nextInt();
-        
-            int caluclate = a/b;
-        
-            System.out.println("the vlaue is : " + caluclate);}
-            catch(InputMismatchException Exception ){
-                System.out.println("please provide required input!");
-                sc.next();
-        
-            }
-            catch(ArithmeticException Exception){
-                System.out.println("oops you divdied it by zero!");
-            }
-        
+        boolean flag = true;
+        while (flag) {
+            System.out.println("enter index: ");
+            try {
+            int ind = sc.nextInt();
+                System.out.println("welcome");
+                try {
+                    System.out.println(arr[ind]);
+                    flag = false;
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("exception occured " + e);
+                }
+            } catch (Exception e) {
+                System.out.println("other exception occured level 1 " + e);
+                sc.next(); //it prevents the infinite loop 
+                
             }
         }
+        System.out.println("thanks for coding");
+    }
+
+    
+        //     for(int i =0;i<5;i++){
+            
+        //     System.out.println("enter the number1 : ");
+        //     try{
+        //     int x = sc.nextInt();
+        //     System.out.println("enter the second number : ");
+        //     int y = sc. nextInt();
+        
+        //     int caluclate = a/b;
+        
+        //     System.out.println("the vlaue is : " + caluclate);}
+        //     catch(InputMismatchException Exception ){
+        //         System.out.println("please provide required input!");
+        //         sc.next();
+        
+        //     }
+        //     catch(ArithmeticException Exception){
+        //         System.out.println("oops you divdied it by zero!");
+        //     }
+        
+        //     }
+        // }
         
         }
 
--- ToString.java
+++ ToString.java
@@ -1,89 +1,82 @@
 
 class Kar {
     int noOfDoors;
-        int wheels;
-        String name;
-        int maxSpeed;
-        
-        final int MARKS = 99; //now rhis final keyword means that the variable ha became constant and cant be changed now,the convention of writing the variable of final keyword is by making them "CAPITAL_AND_ADDED_BY_THESE_DASHES"
+    int wheels;
+    String name;
+    int maxSpeed;
 
-        public Kar(int noOfDoors, int wheels, String name, int maxSpeed) {
-            this.noOfDoors = noOfDoors;
-            this.wheels = wheels;
-            this.name = name;
-            this.maxSpeed = maxSpeed;
-        }
+    final int MARKS = 99; // now this final keyword means that the variable has became constant and cant be changed now,the convention of writing the variable of final keyword is by making them "CAPITAL_AND_ADDED_BY_THESE_DASHES"
 
-        @Override
-        public String toString() {  //this toString class is also defined in java and if we dont define it here then that method will be returned hashes!
-            return "Kar [name=" + name + ", maxSpeed=" + maxSpeed + "]";
-        }
-
-       
+    public Kar(int noOfDoors, int wheels, String name, int maxSpeed) {
+        this.noOfDoors = noOfDoors;
+        this.wheels = wheels;
+        this.name = name;
+        this.maxSpeed = maxSpeed;
     }
 
-
-    // make another class student and by the help of toString method prints his credentials
-
-    class Students {
-        String name;
-        int age;
-        int roll;
-        String house;
-        public Students(String name, int age, int roll) {
-            this.name = name;
-            this.age = age;
-            this.roll = roll;
-        }
-       
-        @Override
-        public String toString(){
-            return "Student-details :{name:" +  name
-                     +" , age: "+ age
-                     +" , roll: "+ roll+" }";
-                    
-        }
-        
-    }
-    
-
-    class Dice{
-
-        double value;
-    
-        double roll(){
-            value = Math.ceil(Math.random() * 6);
-    
-            return value;
-        }
-        @Override
-        public String toString() {
-            return "Dice [value=" + value + "]";
-        }
-    
-        
+    @Override
+    public String toString() { // this toString class is also defined in java and if we dont define it here then that method will be returned hashes!
+        return "Kar [name=" + name + ", maxSpeed=" + maxSpeed + "]";
     }
 
-    
+}
 
-    public class ToString {
+// make another class student and by the help of toString method prints his credentials
+
+class Students {
+    String name;
+    int age;
+    int roll;
+    String house;
+
+    public Students(String name, int age, int roll) {
+        this.name = name;
+        this.age = age;
+        this.roll = roll;
+    }
+
+    @Override
+    public String toString() {
+        return "Student-details :{name:" + name
+                + " , age: " + age
+                + " , roll: " + roll + " }";
+
+    }
+
+}
+
+class Dice {
+
+    double value;
+
+    double roll() {
+        value = Math.ceil(Math.random() * 6);
+
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Dice [value=" + value + "]";
+    }
+
+}
+
+public class ToString {
     public static void main(String[] args) {
         Kar cr = new Kar(4, 4, "maruti", 120);
-        System.out.println(cr.toString()); //will print the name of class followed by hashcode because everytime we try to print object,it will call toString method even if you dont mention.,if basically call the methods toString from its own class and print those hexadecimal hashcode stuff.
+        System.out.println(cr.toString()); // will print the name of class followed by hashcode because everytime we try to print object,it will call toString method even if you dont mention.,if basically call the methods toString from its own class and print those hexadecimal hashcode stuff.
 
-        //so what if we define our own toString mthod it will just imply our toString method
+        // so what if we define our own toString mthod it will just imply our toString
+        // method
         System.out.println(cr);
 
+        Students stu = new Students("goul", 12, 15);
 
-        
-		Students stu = new Students("goul" ,12,15);
-
-		System.out.println(stu + "\n\n");  //toString will called
-        
-
+        System.out.println(stu + "\n\n"); // toString will called
 
         Dice die = new Dice();
-     	System.out.println(die.roll());
+        System.out.println(die.roll());
         System.out.println(die);
 
     }
