a4d0f43a0f60efa0021e935128d6c4b983b23979
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-02-14 15:51:08 +0530

    updated

--- Queues.java
+++ Queues.java
@@ -9,12 +9,12 @@ public class Queues {
         queue.add(1); //can give error if elements cannot be added
         queue.offer(2); 
 
-        CollectionUtility.print(queue); //calling the utility class which will print all the elements
+        CollectionUtility.print(queue); //calling the utility class which will print all the elements of the collection
 
         System.out.println(queue.peek());  // the head of the queue is 1 because 1 entered earliest  ( fist in first out)
-        System.out.println(queue.element());  // peek and element both used to view the head element but  element could give Exception and peek cannot
+        System.out.println(queue.element());  // peek and element both used to view the head element but  element could give Exception but peek cannot
 
-        System.out.println(queue.remove());
+        System.out.println(queue.remove()); //remove can throw Exception of the Queue is empty
 
        System.out.println("elements in queue after deletion : "  ); CollectionUtility.print(queue);
 
@@ -26,6 +26,7 @@ public class Queues {
 
        System.out.println(queue.poll()); //doesnot throw Exception...will print null if the que is empty!
 
+       
      
     }
 }
