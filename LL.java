2923ec49363eeda48753130d2f664a9c5fc4d001
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-02-15 01:13:06 +0530

    updated

---
+++ LL.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+
+public class LL {
+    
+    class Node{
+        String data;
+        Node next;
+
+        Node(String data){
+            this.data =data;
+            this.next = null;  
+        }
+    }
+
+    //add -first,last
+    
+        public static void main(String[] args) {
+            LL list = new LL();
+            LinkedList l1 = new LinkedList<>();
+
+        
+    }
+}
