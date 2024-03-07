8d3b7cb904e656491c6a3fdc01a0ba6533d1346a
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-03-07 14:56:02 +0530

    newly added

--- SyncronizeThread.java
+++ SyncronizeThread.java
@@ -1,9 +1,11 @@
 class Counter {
     private int count = 0;
 
-    public void increment() {
+    public synchronized void increment() {
         count++;
     }
+
+    // by using syncronized : helps to make precise calculations and ensures that only one thread can execute a block o code at a time
 
     public int getCount() {
         return count;
@@ -18,11 +20,11 @@ class Runcounter extends Thread {
         this.counter = counter;
     }
 
-   public void run(){
-     for(int i=0;i<10000;i++){
-        counter.increment();
-     }
-     }
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            counter.increment();
+        }
+    }
 }
 
 public class SyncronizeThread {
@@ -30,28 +32,31 @@ public class SyncronizeThread {
 
         double startTime = System.currentTimeMillis();
         Counter c1 = new Counter();
-        
+
         Runcounter c2 = new Runcounter(c1);
         Runcounter c3 = new Runcounter(c1);
 
-        c2.start();
-        c2.join();
-        c3.start();
-        c3.join();
+        // c2.start(); // we can also use this method to avoid irregularities but there
+        // is no benifit of multithreading here!
+        // c2.join();
+        // c3.start();
+        // c3.join();
 
-        // try {
-            
-             //c2.join and c3.join should be in this trycatch block
-             
-        // } catch (InterruptedException e) {
-        //     e.printStackTrace();
-        //     System.out.println("interrupted!!!");
-        // }
-       
+        c2.start();
+        c3.start();
+
+        try {
+
+            c2.join(); // and
+            c3.join(); // should be in this trycatch block
+
+        } catch (InterruptedException e) {
+            System.out.println("interrupted!!!");
+        }
+
         double endTime = System.currentTimeMillis();
 
-        System.out.printf("%d : time-taken  %f",c1.getCount(),(endTime-startTime));
-        
+        System.out.printf("%d : time-taken  %f", c1.getCount(), (endTime - startTime));
 
     }
 
