7598254b021b74d9f0ce8fcfdec74d9ace26d51a
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-03-20 07:07:40 +0530

    added

---
+++ Leetcode/IsAnagram.java
@@ -0,0 +1,17 @@
+package Leetcode;
+
+import java.util.Arrays;
+
+public class IsAnagram {
+    public boolean isAnagram(String s, String t) {
+        
+        char[] char1 = s.toCharArray();
+        char[] char2 = t.toCharArray();
+        Arrays.sort(char1);
+        Arrays.sort(char2);
+        if(Arrays.equals(char1, char2)){
+            return true;
+        }
+        else return false;
+    }
+}
--- Leetcode/StringReverse.java
+++ Leetcode/StringReverse.java
@@ -8,9 +8,29 @@ public class StringReverse {
              rev+=words[i];
             }
             else rev+=words[i]+" ";
-        }
+        } 
         return rev;
     }
 
     
+    //optimal approach using string builder
+
+    // class Solution {
+    //     public String reverseWords(String s) {
+    //         String[] words = s.trim().split("\\s+");
+    //         StringBuilder rev = new StringBuilder();
+            
+    //         for (int i = words.length - 1; i >= 0; i--) {
+    //             rev.append(words[i]);
+    //             if (i > 0) {
+    //                 rev.append(" ");
+    //             }
+    //         }
+            
+    //         return rev.toString();
+    //     }
+    // }
+    
+
+
 }
