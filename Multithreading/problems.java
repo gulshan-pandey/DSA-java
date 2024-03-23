12b6202d331350e0a33e96fb44e23ed8df4f33e6
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-03-23 16:26:03 +0530

    added problems

--- Multithreading/problems.java
+++ Multithreading/problems.java
@@ -1,52 +1,117 @@
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
+import javax.management.RuntimeErrorException;
 
- 
 //Wap create a fixed thred pool with a specified number of threads using fixedThread,submit multiple tasks to the executor,where each task should print the current thread's name and sleep for a random time between 100 and 500 millisec.
 
-class que2 implements Runnable{
-private int number;
+class que2 implements Runnable {
+    private int number;
 
-  public que2(int number) {
-    this.number = number;
-}
-
-public void run(){
-
-    for(int i = 1;i<=10;i++){
-            try {
-                Thread.sleep((long)(Math.random()*400)+100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(Thread.currentThread().getName() + number);
-        }
-}
-
-
-
-}
-
-
-
-
-
-public class problems{
-    public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(5);
-        que2 q1 = new que2(1);
-        que2 q12= new que2(2);
-        que2 q13 = new que2(3);
-        que2 q14 = new que2(4);
-        que2 q15 = new que2(5);
-            executor.submit(q1);
-            executor.submit(q12);
-            executor.submit(q13);
-            executor.submit(q14);
-            executor.submit(q15);
-             //this is wrapped in try with resoucess in which twe dont need to shutdown the executor ...try with resourse will take care of this
-        }
+    public que2(int number) {
+        this.number = number;
     }
+
+    private int getrandom() {
+        double random = Math.random() * 5 + 1;
+        return (int) random;
+    }
+
+    public void run() {
+
+        System.out.println("started thread : " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(getrandom() * 1000);
+        } catch (InterruptedException e) {
+
+        }
+        System.out.println("ended Thread: " + Thread.currentThread().getName() + number);
+
+    }
+
+}
+
+
+// Que2-- WAP uses an executor service to execute multiple Callable tasks.Each task should calculate and return the factorial of a number provided to it.Use Future objects to receive th results of the calcultaiopns.Aftwer all tasks are submitted,retrieve the result from the future,print them,and ensure the execurtor service sutsdown correctly!
+
+
+
+
+class FetchFactorial implements Callable<Integer>{
+    
+    private int num;
+    
+    public FetchFactorial(int n){
+        this.num= n;
+    }
+    
+    public int fact(int num){
+        int fact =1;
+        for(int i=num;i>=1;i--){
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+            }
+            fact= num *(num-1);
+        }
+        return fact;
+    }
+
+   @Override
+    public Integer call() throws InterruptedException{
+       
+  
+        System.out.println("getting the factorial from the server "+ num+ ":");
+        Thread.sleep(100);
+        return fact(num);
+    }
+
+}
+
+
+
+
+
+
+public class problems {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+        // try {
+        //     ExecutorService executor = Executors.newFixedThreadPool(5);
+        //     for (int i = 0; i < 10; i++) {
+        //         que2 task = new que2(i);
+        //         executor.submit(task);
+        //     }
+        //     // this is wrapped in try with resoucess in which twe dont need to shutdown the
+        //     // executor ...try with resourse will take care of this
+        //     if (!executor.awaitTermination(4, TimeUnit.SECONDS)) {
+        //         System.out.println("emergency shuttingdown");
+        //         executor.shutdownNow();
+        //     }
+        // } catch (Error e) {
+        //     throw new RuntimeErrorException(e);
+        // }
+
+
+
+
+       ExecutorService services =Executors.newFixedThreadPool(5);
+       int j=19;
+       while(j>2){
+          FetchFactorial fxt = new FetchFactorial(j);
+          Future<Integer> answer= services.submit(fxt);
+          System.out.println(answer.get());
+          j=j-2;
+       }
+
+       services.shutdown();
+       
+      
+
+    }
+}
