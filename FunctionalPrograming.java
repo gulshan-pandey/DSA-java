5798e791ce6e380177979cb1217c7841f1aeee61
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-03-24 14:24:59 +0530

    updated

--- FunctionalPrograming.java
+++ FunctionalPrograming.java
@@ -7,15 +7,35 @@ public class FunctionalPrograming {
         
     List <String> fruits = List.of("apple","banana","grapes","peach","orange","date");
 
-    // for(String fruit: fruits){        //aam zindagi
+    // for(String fruit: fruits){                   //traditional way
     //     System.out.println(fruit);
     // }
 
-    fruits.stream().forEach(new Consumer<String>(){
-        public void accept(String fruit){
+
+System.out.println("printing using the Streams,and redefining accept method:--");
+
+    fruits.stream().forEach(new Consumer<String>(){    //here we defined the anonymous inner class of Consumer interface and overrided the accept method
+        @Override
+        public void accept(String fruit){       // accept method is used to perform any operation on the given input
             System.out.println(fruit);
         }
     });  //forEach expects the object of the consumer interface which has only one medthod called {accept()} which is a generic method
+
+
+
+    //ALTERNATIVELY-----------------------------------------------------
+   
+    //the above accept() method can be used as lambda expression for simplicity
+    
+    System.out.println("\n printing using lambda expression within th e:");
+
+    fruits.stream().forEach(fruit->System.out.println(fruit));
+    
+    
+    System.out.println("\n printing using lambda expression within th e:");
+
+    fruits.stream().forEach(fruit->System.out.println(fruit.toUpperCase()));
+
 
 
 
