92840fbb3f749ce180113d2b1730d50b30d32494
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-03-24 17:02:38 +0530

    updated

--- AdvanceJava/FunctionalPrograming.java
+++ AdvanceJava/FunctionalPrograming.java
@@ -1,5 +1,10 @@
+package AdvanceJava;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FunctionalPrograming {
     
@@ -23,13 +28,13 @@ System.out.println("printing using the Streams,and redefining accept method:--")
 
 
 
-    //ALTERNATIVELY-----------------------------------------------------
+    //ALTERNATIVELY----------------------------------------------------- using lambda expression
    
     //the above accept() method can be used as lambda expression for simplicity
     
     System.out.println("\n printing using lambda expression within th e:");
 
-    fruits.stream().forEach(fruit->System.out.println(fruit));
+    fruits.stream().forEach(fruit->System.out.println(fruit));     // here forEach is a terminal operation,Without a terminal operation, intermediate operations will not be executed, and the stream will not perform any processing. 
     
     
     System.out.println("\n printing using lambda expression within th e:");
@@ -39,7 +44,15 @@ System.out.println("printing using the Streams,and redefining accept method:--")
 
 
 
+    List<Integer> numbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
 
-
+    
+    List<Integer> evenNumbers = numbers.stream()
+                                       .map(num -> num * num)
+                                       .collect(Collectors.toList());   //collect is terminal method,Accumulates the elements of this stream into a List
+    
+    for (Integer num : evenNumbers) {
+        System.out.println(num);
+    }
 }
 }
