5e63c3be83492db1de80c0d96e14645c4956269d
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-03-26 22:55:48 +0530

    added

--- AdvanceJava/FunctionalPrograming.java
+++ AdvanceJava/FunctionalPrograming.java
@@ -86,5 +86,9 @@ System.out.println("printing using the Streams,and redefining accept method:--")
     System.out.println(DistinctList);
 
 
+
+
 }
+//WAP to create a list of employees with nama and salary fields,write a comparator that sorts employees by salary.then use this comparator to sort your list using the sort stream operation
+
 }
