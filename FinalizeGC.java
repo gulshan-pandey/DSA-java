2fa4d146cd0515db261beeb7977235a265df5f1d
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-03-30 19:32:06 +0530

    updated

--- FinalizeGC.java
+++ FinalizeGC.java
@@ -2,16 +2,16 @@ public class FinalizeGC {
  String name ;
  int roll;
 @Override
-protected void finalize() throws Throwable {
+protected void finalize() throws Throwable {             //not that ...this finalize method runs automatically by the system whenever it need to garbge collection,also it can be done forcefully by calling System.gc() {depricated method in java 9}
     System.out.println("the memory was cleared by Garbage collection!");
 }
     
 public static void main(String[] args) {
     
   FinalizeGC obj;
-  for(int i =0;i<10;i++){
+  for(int i =0;i<1000000;i++){
     obj = new FinalizeGC();
-    // obj = null;               // maakin the object unrefrenced ,this is optional to do
+    // obj = null;               // makin the object unrefrenced ,this is optional to do
   }
 
   // System.gc();    // the method to forcefully run the garbage collection , this is also optional to do bcz jvm do this automatically
