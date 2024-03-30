fabc7e0e724235d5ff396fc3cba0a67249c73506
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-03-30 19:32:52 +0530

    updated

--- FinalizeGC.java
+++ FinalizeGC.java
@@ -10,8 +10,9 @@ public static void main(String[] args) {
     
   FinalizeGC obj;
   for(int i =0;i<1000000;i++){
-    obj = new FinalizeGC();
-    // obj = null;               // makin the object unrefrenced ,this is optional to do
+    obj = new FinalizeGC();    //assigning new objects so that older refrences get destroyed
+
+    // obj = null;               // making the object unrefrenced ,this is optional to do
   }
 
   // System.gc();    // the method to forcefully run the garbage collection , this is also optional to do bcz jvm do this automatically
