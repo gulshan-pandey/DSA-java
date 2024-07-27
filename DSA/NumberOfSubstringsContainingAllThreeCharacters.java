5fee144bc1916b6a14f203156f9f6d03dbc6ab13
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-07-27 21:59:22 +0530

    Strings-medium

--- DSA/NumberOfSubstringsContainingAllThreeCharacters.java
+++ DSA/NumberOfSubstringsContainingAllThreeCharacters.java
@@ -36,8 +36,6 @@ public class NumberOfSubstringsContainingAllThreeCharacters {
 
 
 
-
-
 // class Solution {            //brute force gives TLE
 //     public int numberOfSubstrings(String s) {
 //         int count = 0;
@@ -55,43 +53,43 @@ public class NumberOfSubstringsContainingAllThreeCharacters {
 
 
 
-class Solution {
-    public int numberOfSubstrings(String s) {
-        int count = 0;
-        int aCount = 0, bCount = 0, cCount = 0;
-        int start = 0;
+// class Solution {
+//     public int numberOfSubstrings(String s) {
+//         int count = 0;
+//         int aCount = 0, bCount = 0, cCount = 0;
+//         int start = 0;
 
-        for (int end = 0; end < s.length(); end++) {
-            char ch = s.charAt(end);
-            if (ch == 'a') {
-                aCount++;
-            } else if (ch == 'b') {
-                bCount++;
-            } else if (ch == 'c') {
-                cCount++;
-            }
+//         for (int end = 0; end < s.length(); end++) {
+//             char ch = s.charAt(end);
+//             if (ch == 'a') {
+//                 aCount++;
+//             } else if (ch == 'b') {
+//                 bCount++;
+//             } else if (ch == 'c') {
+//                 cCount++;
+//             }
 
-            while (aCount > 0 && bCount > 0 && cCount > 0) {
-                count += s.length() - end; // All substrings from start to end are valid
-                char startChar = s.charAt(start);
-                if (startChar == 'a') {
-                    aCount--;
-                } else if (startChar == 'b') {
-                    bCount--;
-                } else if (startChar == 'c') {
-                    cCount--;
-                }
-                start++;
-            }
-        }
-        return count;
-    }
-}
-
-
+//             while (aCount > 0 && bCount > 0 && cCount > 0) {
+//                 count += s.length() - end; // All substrings from start to end are valid
+//                 char startChar = s.charAt(start);
+//                 if (startChar == 'a') {
+//                     aCount--;
+//                 } else if (startChar == 'b') {
+//                     bCount--;
+//                 } else if (startChar == 'c') {
+//                     cCount--;
+//                 }
+//                 start++;
+//             }
+//         }
+//         return count;
+//     }
+// }
 
 
-// class Solution {                                             // yet to revise--- not fully understood
+
+
+// class Solution {                                   // yet to revise--- not fully understood
 //     public int numberOfSubstrings(String s) {
 //         int[] lastSeen = {-1, -1, -1};
 //         int count = 0;
@@ -107,3 +105,27 @@ class Solution {
 
 
 
+
+// class Solution {
+//     public int numberOfSubstrings(String s) {
+//         int count = 0;
+//         int[] charCount = {0, 0, 0};  // to store the count of 'a', 'b', 'c'
+//         int left = 0;
+
+//         for (int right = 0; right < s.length(); right++) {
+//             charCount[s.charAt(right) - 'a']++;
+            
+//             // When all characters 'a', 'b', 'c' are present in the window
+//             while (charCount[0] > 0 && charCount[1] > 0 && charCount[2] > 0) {
+//                 // All substrings from left to right are valid
+//                 count += s.length() - right;
+                
+//                 // Shrink the window from the left
+//                 charCount[s.charAt(left) - 'a']--;
+//                 left++;
+//             }
+//         }
+
+//         return count;
+//     }
+// }
