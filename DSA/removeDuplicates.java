4a5966b97206da1a0aba042e35aa9b3e12e83242
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-08-01 16:22:30 +0530

    Arrays

--- DSA/removeDuplicates.java
+++ DSA/removeDuplicates.java
@@ -2,10 +2,11 @@ package DSA;
 
 public class removeDuplicates {
     
+    // Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
 }
 
 
-// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
 
 
 // class Solution {
