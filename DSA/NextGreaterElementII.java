36dab3d8bb74bcc1720dc75c5ed7cb84e5b220cf
Gulshan <125683164+gulshan-pandey@users.noreply.github.com>
2024-08-26 11:13:10 +0530

    Circular Array, Monotonic Stack

--- DSA/NextGreaterElementII.java
+++ DSA/NextGreaterElementII.java
@@ -10,7 +10,6 @@ public class NextGreaterElementII {
 
         The next greater number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this number.
 
-        
 
         Example 1:
 
