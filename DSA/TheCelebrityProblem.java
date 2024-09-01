7b76bbd1a4887cf9b50c6aa1e95648ed36126541
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-09-01 16:16:29 +0530

    2D array, Matrix

--- DSA/TheCelebrityProblem.java
+++ DSA/TheCelebrityProblem.java
@@ -1,6 +1,9 @@
 package DSA;
 public class TheCelebrityProblem {
+
     /**
+     *    https://www.geeksforgeeks.org/problems/the-celebrity-problem/1
+     * 
      * A celebrity is a person who is known to all but does not know anyone at a party. A party is being organized by some people.  A square matrix mat is used to represent people at the party such that if an element of row i and column j is set to 1 it means ith person knows jth person. You need to return the index of the celebrity in the party, if the celebrity does not exist, return -1.
 
         Note: Follow 0-based indexing.
