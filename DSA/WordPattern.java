6435b6cdd77c609d4b4b41a3caf42f82345ec5db
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-09-17 21:29:46 +0530

    Hashmap, Strings

---
+++ DSA/WordPattern.java
@@ -0,0 +1,89 @@
+package DSA;
+
+import java.util.HashMap;
+
+public class WordPattern {
+    
+    /**
+     * Given a pattern and a string s, find if s follows the same pattern.
+
+        Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s. Specifically:
+
+        Each letter in pattern maps to exactly one unique word in s.
+        Each unique word in s maps to exactly one letter in pattern.
+        No two letters map to the same word, and no two words map to the same letter.
+        
+
+        Example 1:
+
+        Input: pattern = "abba", s = "dog cat cat dog"
+
+        Output: true
+
+        Explanation:
+
+        The bijection can be established as:
+
+        'a' maps to "dog".
+        'b' maps to "cat".
+        Example 2:
+
+        Input: pattern = "abba", s = "dog cat cat fish"
+
+        Output: false
+
+        Example 3:
+
+        Input: pattern = "aaaa", s = "dog cat cat dog"
+
+        Output: false
+
+ 
+     * 
+     */
+}
+
+
+
+
+
+
+
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        HashMap<Character, String> map = new HashMap<>();
+        HashMap<String, Character> map2 = new HashMap<>();
+
+        String[] words = s.split(" ");
+        if(words.length!= pattern.length()) return false;     // Number of words and characters must match
+
+        for(int i=0; i<words.length; i++){
+            char ch = pattern.charAt(i);
+            String str = words[i];
+
+            // Check if 'ch' is already mapped to a different word
+            if(map.containsKey(ch)){
+                if(!map.get(ch).equals(str)) return false;                  // Pattern doesn't match            
+            }else{
+                map.put(ch,str);
+            }
+        
+
+            // Check if 'word' is already mapped to a different character
+            if(map2.containsKey(str)){
+                if(!map2.get(str).equals(ch)) return false;                 // Pattern doesn't match
+            }else{
+                map2.put(str,ch);
+            }
+        }
+        
+
+        return true;   // All checks passed
+
+
+    }
+}
+
+
+
