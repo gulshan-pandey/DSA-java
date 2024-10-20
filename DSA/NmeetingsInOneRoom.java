c646f78000992d9aa811ab11e8df05e7f30328c8
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-10-20 17:46:38 +0530

    Greedy

--- DSA/NmeetingsInOneRoom.java
+++ DSA/NmeetingsInOneRoom.java
@@ -1,4 +1,7 @@
 package DSA;
+
+import java.util.Arrays;
+
 public class NmeetingsInOneRoom {
     /**
      * You are given timings of n meetings in the form of (start[i], end[i]) where start[i] is the start time of meeting i and end[i] is the finish time of meeting i. Return the maximum number of meetings that can be accommodated in a single meeting room, when only one meeting can be held in the meeting room at a particular time. 
