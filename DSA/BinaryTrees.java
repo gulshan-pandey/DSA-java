fb96b28281fd5f67b20524ede1fd6a0c122d5ce9
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-10-20 23:17:30 +0530

    Binary Search, Median

---
+++ DSA/BinaryTrees.java
@@ -0,0 +1,21 @@
+package DSA;
+
+public class BinaryTrees {
+    
+    public BinaryTrees(){
+
+    }
+
+    private static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value){
+            this.value =value;
+        }
+    }
+
+    private Node root;
+    
+}
