e9cc78b7a25a4d4e2ea32fa4ef68cafac4c05ddb
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-10-20 17:40:23 +0530

    Binary Search on duplicates

--- DSA/FindFirstAndLastPositionOfElementInSortedArray.java
+++ DSA/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -1,5 +1,5 @@
 package DSA;
-public class Find First and LastPositionOfElementInSortedArray {
+public class FindFirstAndLastPositionOfElementInSortedArray {
     /**
      * 
      * Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
