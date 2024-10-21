8c3b93ede01b8f78585473b6227b6706bf3fda61
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-10-21 13:58:10 +0530

    A Full proof , Unbreakable Singleton class

--- Singleton.java
+++ Singleton.java
@@ -1,3 +1,4 @@
+import java.io.Serializable;
 // the aim is that make this class like that only one object is to be allowed
 
 public class Singleton {
@@ -18,6 +19,17 @@ public class Singleton {
     
 }
 
+
+
+/**
+ * This singleton class can be broken by below aspects
+ * Multithreading
+ * clonable
+ * serializable
+ * reflection api
+ * 
+ */
+
 class template{
 
     public static void main(String[] args) {
@@ -33,5 +45,43 @@ class template{
         System.out.println(obj1);
         System.out.println(obj2);
         System.out.println(obj3);
+    }
+}
+
+
+
+// Making full proof singleton 
+
+
+
+ class FullSingleton implements Serializable, Cloneable {
+
+    // private constructor to prevent instantiation from outside
+    private FullSingleton() {
+        // Prevent reflection from breaking FullSingleton
+        if (SingletonHelper.INSTANCE != null) {
+            throw new RuntimeException("Instance already exists! Use getInstance() method to get the single instance.");
+        }
+    }
+
+    // Bill Pugh FullSingleton Design (Inner static helper class)
+    private static class SingletonHelper {
+        private static final FullSingleton INSTANCE = new FullSingleton();
+    }
+
+    // Global access point to the FullSingleton instance
+    public static FullSingleton getInstance() {
+        return SingletonHelper.INSTANCE;
+    }
+
+    // To prevent creating a new instance during deserialization
+    protected Object readResolve() {
+        return SingletonHelper.INSTANCE;
+    }
+
+    // To prevent cloning of the Singleton instance
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        throw new CloneNotSupportedException("Cloning of this singleton is not allowed!");
     }
 }
