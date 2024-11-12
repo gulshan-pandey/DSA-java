93071fed77fddcdc13940396e31e4f4eee2e2855
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-11-12 10:20:46 +0530

    2pointers, LinkedList

--- DSA/DeleteTheMiddleNodeOfALinkedList.java
+++ DSA/DeleteTheMiddleNodeOfALinkedList.java
@@ -79,3 +79,30 @@ public class DeleteTheMiddleNodeOfALinkedList {
 //         return head;
 //     }
 // }
+
+
+
+
+
+// Alternative approach
+
+
+ 
+// class Solution {
+//     public ListNode deleteMiddle(ListNode head) {
+
+//     if (head == null || head.next == null) {
+//             return null;
+//         }
+//         ListNode slow = head;
+//         ListNode fast = head;
+//         while (fast.next.next != null && fast.next.next.next!= null) {
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+       
+//        slow.next = slow.next.next;
+
+//         return head;
+//     }
+// }
