df3b912637b464c08ae9ca393cdd52b418677bd4
Gulshan <125683164+gulshan-pandey@users.noreply.github.com>
2024-11-26 17:46:21 +0530

    String, Xor

--- DSA/ReverseString.java
+++ DSA/ReverseString.java
@@ -6,19 +6,19 @@ public class ReverseString {
 
 
 
+
+
 class Solution {
     public void reverseString(char[] s) {
         for (int i = 0; i < s.length / 2; i++) {
             int j = s.length - 1 - i;
-            if (i != j) {
                 s[i] ^= s[j];
                 s[j] ^= s[i];
                 s[i] ^= s[j];
-            }
+            
         }
     }
 }
-
 
 
 
