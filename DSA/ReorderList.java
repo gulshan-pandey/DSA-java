e3af99fde076a45b087e3b49ecbb39a357e9c4e8
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-12-15 13:45:21 +0530

    LinkedList

---
+++ DSA/ReorderList.java
@@ -0,0 +1,80 @@
+package DSA;
+public class  ReorderList {
+    /**
+     * You are given the head of a singly linked-list. The list can be represented as:
+
+        L0 → L1 → … → Ln - 1 → Ln
+        Reorder the list to be on the following form:
+
+        L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+        You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+        
+
+        Example 1:
+
+
+        Input: head = [1,2,3,4]
+        Output: [1,4,2,3]
+        Example 2:
+
+
+        Input: head = [1,2,3,4,5]
+        Output: [1,5,2,4,3]
+        
+     * 
+     */
+}
+
+
+
+/**
+ * Approach:
+ * 
+ *  1. Find the middle of the list by slow, fast approach
+ *  2. Reverse the second half of the list
+ *  3. Merge the two halves accordingly
+ */
+
+
+// class Solution {
+//     public void reorderList(ListNode head) {
+//         if (head == null || head.next == null) return;
+
+//         ListNode slow =head;
+//         ListNode fast = head;
+        
+//         while(fast!=null && fast.next!=null){
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+
+//         ListNode prev = null;
+//         ListNode curr = slow.next;
+//         slow.next =null;        // Break the link between the two halves
+
+//         while(curr!=null){      // Reverse the second half of the list
+//             ListNode next = curr.next;
+//             curr.next =prev;
+//             prev = curr;
+//             curr = next;
+//         }
+
+//         ListNode first = head;
+//         ListNode second = prev;
+
+//         while(second!=null){            // merging
+//             ListNode temp1 =first.next;
+//             ListNode temp2 =second.next;
+
+//             first.next = second;
+//             second.next =temp1;
+
+//             first = temp1;
+//             second = temp2;
+
+//         }
+
+
+//     }
+// }
