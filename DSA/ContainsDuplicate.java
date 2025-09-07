94410773d5f59816824dcd83dbca6f7376333cf4
Gulshan <125683164+gulshan-pandey@users.noreply.github.com>
2025-09-07 22:51:17 +0530

    arrays, hashmap, hashset, sorting

--- DSA/ContainsDuplicate.java
+++ DSA/ContainsDuplicate.java
@@ -20,6 +20,35 @@ public class ContainsDuplicate {
     
 }
 
+// by frequency map
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+//         for (int num : nums) {
+//             frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+//             if (frequencyMap.get(num) > 1) {
+//                 return true; // Duplicate found
+//             }
+//         }
+//         return false; // No duplicates
+//     }
+// }
+
+
+// by sorting and comparing the adjacents
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         Arrays.sort(nums); // Sort the array
+//         for (int i = 1; i < nums.length; i++) {
+//             if (nums[i] == nums[i - 1]) {
+//                 return true; // Duplicate found
+//             }
+//         }
+//         return false; // No duplicates
+//     }
+// }
 
 
 
